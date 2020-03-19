e2957fb29fbf3f889e45f859c6184e3e662d97c2
skander <skanders@live.fr>
2020-03-19 19:15:34 +0100

    config project

---
+++ src/main/java/com/worldcup/BackWorldCup/BackWorldCupApplication.java
@@ -0,0 +1,12 @@
+package com.worldcup.BackWorldCup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackWorldCupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackWorldCupApplication.class, args);
+	}
+}
---
+++ src/test/java/com/worldcup/BackWorldCup/BackWorldCupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldcup.BackWorldCup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackWorldCupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
